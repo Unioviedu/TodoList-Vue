14df221839c73c176bec72a9de9dcec8c0c7c410
rgarciaf <raul.garcia-fernandez-external@capgemini.com>
2019-11-21 12:47:22 +0100

    Terminadas Vistas iniciales

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template src="./template.html"></template>
+<script>
+export default {
+  name: 'home',
+  components: {
+  },
+  methods: {
+    anadirTarea (_this) {
+      this.$router.push('/task')
+    }
+  }
+}
+</script>
+<style lang="scss" src="./style.scss"></style>
---
+++ src/views/task/index.vue
@@ -0,0 +1,14 @@
+<template src="./template.html"></template>
+<script>
+export default {
+  name: 'task',
+  components: {
+  },
+  methods: {
+    anadirTarea (_this) {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+<style lang="scss" src="./style.scss"></style>
