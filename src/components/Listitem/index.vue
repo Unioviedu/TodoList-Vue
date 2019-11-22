3a98488c6cb013e24e4933a3f2fb9b13fae068f4
David García <davgg1983@gmail.com>
2019-11-22 09:45:57 +0100

    primeros cambios

---
+++ src/components/Listitem/index.vue
@@ -0,0 +1,19 @@
+<template src="./template.html"></template>
+
+<script>
+  export default {
+    name: 'ListItem',
+
+    props: {
+      title: String,
+      description: String,
+      done: {
+        type: Boolean,
+        default: false
+      },
+      id: Number
+    }
+  }
+</script>
+
+<style lang="scss" src="./styles.scss" scoped></style>
