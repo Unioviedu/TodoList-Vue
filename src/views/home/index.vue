e40152c439519d0a2183b9453ea467dfa8afa08f
David García <davgg1983@gmail.com>
2019-11-22 10:05:22 +0100

    primeros cambios

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,18 +1,46 @@
 <template src="./template.html"></template>
 <script>
 
-import ListItem from '../../components/Listitem/index'
+  import ListItem from '../../components/Listitem/index'
 
-export default {
-  name: 'home',
-  components: {
-    ListItem
-  },
-  methods: {
-    anadirTarea (_this) {
-      this.$router.push('/task')
+  export default {
+    name: 'home',
+    components: {
+      ListItem
+    },
+
+    data () {
+      return {
+        tasks: [
+          {
+            id: 1,
+            title: 'Tarea 1',
+            description: 'Esta es la tarea 1 creada para simular una lista de tareas con VUE'
+          },
+          {
+            id: 2,
+            title: 'Tarea 2',
+            description: 'Esta es la tarea 2 creada para simular una lista de tareas con VUE'
+          },
+          {
+            id: 3,
+            title: 'Tarea 3',
+            description: 'Esta es la tarea 3 creada para simular una lista de tareas con VUE'
+          },
+          {
+            id: 4,
+            title: 'Tarea 4',
+            description: 'Esta es la tarea 4 creada para simular una lista de tareas con VUE'
+          }
+        ]
+      }
+    },
+
+    methods: {
+      anadirTarea (_this) {
+        this.$router.push('/task')
+      }
     }
   }
-}
 </script>
 <style lang="scss" src="./style.scss"></style>
